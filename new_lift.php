b4a5ff041bf7d3956b53c43a20f4283beea41c7c
terryproctor <terryproctor2015@outlook.com>
2022-09-25 00:15:06 +0100

    radio buttons causing issues

--- new_lift.php
+++ new_lift.php
@@ -1,8 +1,35 @@
+
 <?php
 $new_lift = $_POST['new_lift_type'];
 $new_weight = $_POST['new_lift_weight'];
-$new_reps = $_POST['new_lift_weight'];
+$new_reps = $_POST['new_lift_reps'];
 $new_one_rep_max = $new_weight * (1 + ($new_reps/30));
 
+      $host="localhost"; 
+      $user="root"; 
+      $password="";
+      $database="lift"; 
+      
+      $conn= new mysqli($host, $user, $password, $database);
 
+      echo $new_lift, $new_weight, $new_reps;
+
+$sql = "INSERT INTO lifts(weight, reps) 
+        VALUES ($new_weight, $new_reps) 
+            ;"
+        ;
+
+        
+            
+           
+            
+           if($conn->query($sql) === TRUE){
+            echo "Record Added Sucessfully";
+           }
+           else
+           {
+            echo "Error" . $sql . "<br/>" . $conn->error;
+           }
+           $conn->close();
+       
 ?>
